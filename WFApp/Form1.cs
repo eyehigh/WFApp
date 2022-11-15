a3568119c147cd306bb3a8a4d7e88305e8a7ddac
Lee DongHun <eye1258@naver.com>
2022-11-15 17:54:32 +0900

    기초 쿼리 및 db작성

--- WFApp/Form1.cs
+++ WFApp/Form1.cs
@@ -1,10 +1,44 @@
+using System.Data;
+using WFApp.Module;
+
 namespace WFApp
 {
     public partial class Form1 : Form
     {
+        SqliteDB sqliteDB;
         public Form1()
         {
             InitializeComponent();
+            sqliteDB = new SqliteDB();
+            sqliteDB.Connect();
+            FUNC1();
+            string select = @"SELECT * FROM employee;";
+            DataTable dt = sqliteDB.SelectDT(select);
+            MessageBox.Show(dt.Rows[0]["name"].ToString());
+        }
+
+        public void FUNC1()
+        {
+            string create_test =
+                @"
+                      CREATE TABLE employee 
+                    (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                        name VARCHAR(255), 
+                        department VARCHAR(255), 
+                        position VARCHAR(255),
+                        join_date DATE
+                    );
+                ";
+            //sqliteDB.SimpleQuery(create_test); // 한번만 실행하고 주석처리할것
+            string insert_test =
+               @"
+                    INSERT INTO  employee 
+                     (name, department, position, join_date) 
+                    VALUES 
+	                ('tony park', 'Data Engineering', 'manager', '2015-02-19');
+                ";
+            //sqliteDB.SimpleQuery(insert_test); // 한번만 실행하고 주석처리할것
         }
     }
 }
---
+++ WFApp/Module/DataBaseManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+namespace WFApp.Module
+{
+    internal class DataBaseManager
+    {
+        
+    }
+    public class SqliteDB
+    {
+        //=========================================
+        //CONST
+        const string SQLITE_DB_PATH = @"C:\SideProject\mydb.db";
+
+        //=========================================
+        //PRIVATE
+       
+        private SQLiteConnection conn = null;
+        private SQLiteConnectionStringBuilder connSB = null;
+        private SQLiteTransaction Transaction = null;
+        private SQLiteCommand command = null;
+        //=========================================
+
+        public SqliteDB()
+        {
+            connSB = new SQLiteConnectionStringBuilder();
+            connSB.DataSource = SQLITE_DB_PATH;
+
+            
+        }
+         ~SqliteDB()
+        {
+            conn.Close();
+        }
+        public bool Connect()
+        {
+            bool result = false;
+            conn = new SQLiteConnection(connSB.ConnectionString);
+            try
+            {
+                conn.Open();
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                result = false; 
+            }
+            return result;
+        }
+        public bool BeginTrans()
+        {
+            Transaction = conn.BeginTransaction();
+            return true;
+        }
+        public bool SimpleQuery(string sql)
+        {
+            command = conn.CreateCommand();
+            command.CommandText = sql;
+            command.ExecuteNonQuery();
+            return true;
+        }
+        public DataTable SelectDT(string sql)
+        {
+            DataTable dt = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(sql, conn);
+            adapter.Fill(dt);
+            return dt;
+        }
+        public bool CommitTrans()
+        {
+            Transaction.Commit();
+            return true;
+        }
+        public bool RollBack()
+        {
+            Transaction.Rollback();
+            return true;
+        }
+    }
+}
